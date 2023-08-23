d4115f4b88f555f1b89ce1ffa5604251cc0775b6
Abhi19112001 <143005807+Abhi19112001@users.noreply.github.com>
2023-08-23 22:28:55 +0530

    Update Program1.java

--- Demo/src/git/Program1.java
+++ Demo/src/git/Program1.java
@@ -4,6 +4,9 @@ public class Program1 {
 public void m1() {
 	System.out.println("M1 method");
 }
+public void m2() {
+	System.out.println("M2 method");
+}
 public static void main(String[] args) {
 	System.out.println("Hello");
 }
